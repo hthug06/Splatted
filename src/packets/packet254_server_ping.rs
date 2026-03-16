d989468435accde2d5d9eb250c0d8599e679ec32
ht06 <hthug06@gmail.com>
2026-03-16 19:17:02 +0100

    fix: ClientPacket function

--- src/packets/packet254_server_ping.rs
+++ src/packets/packet254_server_ping.rs
@@ -1,9 +1,9 @@
 use crate::packets::ClientPacket;
-
+#[derive(Default)]
 pub struct ServerPing;
 
 impl ClientPacket for ServerPing {
-    fn write() -> Vec<u8> {
+    fn write(&self) -> Vec<u8> {
         vec![254, 1]
     }
 }
