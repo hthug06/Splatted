0df089754d19322d5b29955fba168df37b1c4586
ht06 <hthug06@gmail.com>
2026-05-06 11:38:11 +0200

    fix(PlayerInfoPacket): ping is i16 and not i32

--- src/packets/packet201_player_info.rs
+++ src/packets/packet201_player_info.rs
@@ -1,6 +1,6 @@
 use crate::network::connection::Encryption;
 use crate::packets::packet_trait::ServerPacket;
-use crate::packets::utils::{read_i32, read_string, read_u8};
+use crate::packets::utils::{read_i16, read_string, read_u8};
 use std::io::Error;
 use tokio::io::BufReader;
 use tokio::net::tcp::OwnedReadHalf;
@@ -9,7 +9,7 @@ use tokio::net::tcp::OwnedReadHalf;
 pub struct PlayerInfo {
     pub name: String,
     pub is_connected: bool,
-    pub ping: i32,
+    pub ping: i16,
 }
 
 impl ServerPacket for PlayerInfo {
@@ -23,7 +23,7 @@ impl ServerPacket for PlayerInfo {
         Ok(Self {
             name: read_string(reader, encryption).await?,
             is_connected: read_u8(reader, encryption).await? != 0,
-            ping: read_i32(reader, encryption).await?,
+            ping: read_i16(reader, encryption).await?,
         })
     }
 }
