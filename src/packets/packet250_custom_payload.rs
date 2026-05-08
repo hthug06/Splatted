b22e570319170ef54530fec96df631748ad75cc4
ht06 <hthug06@gmail.com>
2026-05-08 12:19:41 +0200

    fix(Custom Payload Packet): Handle when the length of the custom payload is 0.

--- src/packets/packet250_custom_payload.rs
+++ src/packets/packet250_custom_payload.rs
@@ -9,7 +9,7 @@ use tokio::net::tcp::OwnedReadHalf;
 pub struct CustomPayloadPacket {
     pub channel: String,
     pub length: i16,
-    pub payload: Vec<u8>,
+    pub payload: Option<Vec<u8>>,
 }
 
 impl ServerPacket for CustomPayloadPacket {
@@ -24,19 +24,27 @@ impl ServerPacket for CustomPayloadPacket {
         let length = read_i16(reader, encryption).await?;
 
         if length > 0 && length < 32767 {
-            let mut payload = Vec::with_capacity(length as usize);
+            let mut payload = vec![0u8; length as usize];
             reader.read_exact(&mut payload).await?;
             encryption.decrypt(&mut payload);
             Ok(Self {
                 channel,
                 length,
-                payload,
+                payload: Some(payload),
+            })
+        }
+        // The size can be equal to 0
+        else if length == 0 {
+            Ok(Self {
+                channel,
+                length,
+                payload: None,
             })
         } else {
             Err(Error::new(
                 std::io::ErrorKind::InvalidData,
                 format!(
-                    "Payload may not be larger than 32k. Actual length: {}",
+                    "Payload size invalid or too large. Actual length: {}",
                     length
                 ),
             ))
