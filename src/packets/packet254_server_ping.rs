ba9af353ae7d2e39b0de77cc281428cedf8292ea
ht06 <hthug06@gmail.com>
2026-05-11 09:36:52 +0200

    fix(Server Ping Packet): Avoid recreating a vec

--- src/packets/packet254_server_ping.rs
+++ src/packets/packet254_server_ping.rs
@@ -1,12 +1,13 @@
 use crate::packets::packet_trait::ClientPacket;
-use bytes::BytesMut;
+use bytes::{BufMut, BytesMut};
 use std::io::Error;
 
 pub struct ServerPingPacket;
 
 impl ClientPacket for ServerPingPacket {
     fn write_to(&self, buffer: &mut BytesMut) -> Result<(), Error> {
-        buffer.extend(vec![254, 1]);
+        buffer.put_u8(254);
+        buffer.put_u8(1);
         Ok(())
     }
 }
