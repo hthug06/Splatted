153fa5471ac2b2281a211787bb537dfbafeff6a1
ht06 <hthug06@gmail.com>
2026-05-11 09:35:48 +0200

    fix: prevent integer overflow for write_byte_array

--- src/packets/io/write.rs
+++ src/packets/io/write.rs
@@ -3,9 +3,19 @@ use bytes::{BufMut, BytesMut};
 
 impl MinecraftWriteExt for BytesMut {
     /// Write a byte array like Minecraft 1.4.7 do (len as u16 in be_byte + data)
-    fn write_byte_array(&mut self, byte_array: &[u8]) {
+    fn write_byte_array(&mut self, byte_array: &[u8]) -> Result<(), std::io::Error> {
+        // Minecraft server use short, so 32_767.
+        // We need to ensure the len of the byte_array is <= 32767 else the server will crash
+        if byte_array.len() > 32_767 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Byte array too big",
+            ));
+        }
         self.extend((byte_array.len() as u16).to_be_bytes());
         self.extend(byte_array);
+
+        Ok(())
     }
 
     /// Add a string to an [u8] &mut self
@@ -18,6 +28,8 @@ impl MinecraftWriteExt for BytesMut {
         let length = utf16_data.len();
 
         //Verify now
+        // Minecraft server use short, so 32_767.
+        // We need to ensure the len of the byte_array is <= 32767 else the server will crash
         if length > 32_767 {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::InvalidData,
